81bc67db8f28083d89e52f205620719bc5ab484c
Patrick Lee Scott <pscott@ghx.com>
2024-10-12 20:46:15 -0500

    feat: implement on

--- models/todo_repository.go
+++ models/todo_repository.go
@@ -1,8 +1,8 @@
 package models
 
-// ToDoRepository inherits from Repository
+// ToDoRepository inherits from Repository and exposes event-emitting behavior
 type ToDoRepository struct {
-	*Repository // Inherits EventEmitter and other repository methods
+	*Repository // Inherits Repository and its behavior, including event handling
 }
 
 // NewToDoRepository initializes a new ToDoRepository
@@ -15,4 +15,9 @@ func NewToDoRepository() *ToDoRepository {
 // SaveToDo saves the ToDo entity and commits events
 func (r *ToDoRepository) SaveToDo(todo *ToDo) {
 	r.Save(todo.Entity)
+}
+
+// On registers an event listener in the underlying EventEmitter
+func (r *ToDoRepository) On(event string, listener func(data interface{})) {
+	r.EventEmitter.On(event, listener)
 }
